5e171f8f20ee60e2aaf25339c90197ccd9dcac12
beefarino <jimchristopher@gmail.com>
2014-06-20 12:54:30 -0400

    interim commit of spike

--- CodeOwls.PowerShell.TFS/Class1.cs
+++ CodeOwls.PowerShell.TFS/Class1.cs
@@ -11,6 +11,8 @@ using CodeOwls.PowerShell.Provider;
 using CodeOwls.PowerShell.Provider.PathNodeProcessors;
 using CodeOwls.PowerShell.Provider.PathNodes;
 using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.Framework.Client;
+using Microsoft.TeamFoundation.Framework.Common;
 
 namespace CodeOwls.TFS
 {
@@ -111,7 +113,68 @@ namespace CodeOwls.TFS
             }
         }
 
-        
+        public override IEnumerable<IPathNode> GetNodeChildren(IProviderContext providerContext)
+        {
+            var nodes = new List<IPathNode>();
+
+            var registry = Server.GetService<ITeamFoundationRegistry>();
+            var collection = registry.ReadEntries("/**");
+            var re = new Regex(@"\/[^\/]+$");
+            var entries = from entry in collection
+                         let key = re.Replace(entry.Path, String.Empty) 
+                         group entry by key;
+
+            var registryNodes = CollateGroups(entries);
+
+            nodes.AddRange(registryNodes.Children);
+            //AddProjectCollectionNodes(nodes);
+
+
+            return nodes;
+        }
+
+        private RegistryEntryCollectionPathNode CollateGroups(IEnumerable<IGrouping<string, RegistryEntry>> entries)
+        {
+            var rootNodes = new RegistryEntryCollectionPathNode("");
+            
+            foreach (var entry in entries)
+            {
+                var segments = entry.Key.Split(new[] {'/'}, StringSplitOptions.RemoveEmptyEntries);
+
+                var nodes = rootNodes.Children;
+                foreach (var segment in segments)
+                {
+                    var node = nodes.FirstOrDefault(n => n.Name == segment);
+                    if (null == node)
+                    {
+                        node = new RegistryEntryCollectionPathNode( segment );
+                        nodes.Add( node );
+                    }
+                    nodes = ((RegistryEntryCollectionPathNode) node).Children;
+                }
+
+                nodes.AddRange( from e in entry select new RegistryEntryPathNode(e) );
+
+            }
+            return rootNodes;
+        }
+
+        private void AddProjectCollectionNodes(List<IPathNode> nodes)
+        {
+            var catalogNode = Server.CatalogNode;
+            var catalogNodes = catalogNode.QueryChildren(new Guid[] {CatalogResourceTypes.ProjectCollection}, false,
+                                                         CatalogQueryOptions.None);
+
+
+            nodes.AddRange(catalogNodes.ToList().ConvertAll(c =>
+                                                                {
+                                                                    var n =
+                                                                        Server.GetTeamProjectCollection(
+                                                                            new Guid(c.Resource.Properties["InstanceId"]));
+                                                                    return new TeamProjectCollectionPathNode(n, c);
+                                                                }));
+        }
+
         public override IPathValue GetNodeValue()
         {
             return new ContainerPathValue( Server, Name );
@@ -120,6 +183,95 @@ namespace CodeOwls.TFS
         public override string Name
         {
             get { return Server.Name; }
+        }
+    }
+
+    public class RegistryEntryCollectionPathNode : PathNodeBase
+    {
+        private readonly List<IPathNode> _children;
+
+        private readonly string _name;
+
+        public RegistryEntryCollectionPathNode(string key)
+        {
+            _name = key;
+            _children = new List<IPathNode>();
+        }
+
+        public override IPathValue GetNodeValue()
+        {
+            return new ContainerPathValue( _name, Name);
+        }
+
+        public override IEnumerable<IPathNode> GetNodeChildren(IProviderContext providerContext)
+        {
+            return Children;
+        }
+
+        public override string Name
+        {
+            get { return _name; }
+        }
+
+        internal List<IPathNode> Children
+        {
+            get { return _children; }
+        }
+    }
+
+    public class RegistryEntryPathNode : PathNodeBase
+    {
+        private readonly RegistryEntry _entry;
+
+        public RegistryEntryPathNode(RegistryEntry entry)
+        {
+            _entry = entry;
+        }
+
+        public override IPathValue GetNodeValue()
+        {
+            return new LeafPathValue( _entry, Name );
+        }
+
+        public override string Name
+        {
+            get { return _entry.Path; }
+        }
+    }
+
+
+    public class TeamProjectCollectionPathNode : CatalogNodePathNode
+    {
+        private readonly TfsTeamProjectCollection _collection;
+        
+        public TeamProjectCollectionPathNode(TfsTeamProjectCollection collection, CatalogNode node) : base(node)
+        {
+            _collection = collection;
+        }
+
+        public override IPathValue GetNodeValue()
+        {
+            return new ContainerPathValue( _collection, Name);
+        }        
+    }
+
+    public class CatalogNodePathNode : PathNodeBase
+    {
+        private readonly CatalogNode _input;
+
+        public CatalogNodePathNode(CatalogNode input)
+        {
+            _input = input;
+        }
+
+        public override IPathValue GetNodeValue()
+        {
+            return new LeafPathValue( _input, Name );
+        }
+
+        public override string Name
+        {
+            get { return _input.Resource.DisplayName; }
         }
     }
 }
